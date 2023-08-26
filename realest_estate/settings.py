b9390cb29211804e06ba46fdd1492507e474d372
Fatih90FM91 <hasan9091.fs@gmail.com>
2023-08-27 01:25:24 +0200

    asdasdasd

--- realest_estate/settings.py
+++ realest_estate/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-_qv)r@@hma&-&kximc_%oefz*mr*vx2t97(0osdkoqlo-orzz3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://backend-realestate-2023-5caa7ebcd402.herokuapp.com/']
 
 
 # Application definition
